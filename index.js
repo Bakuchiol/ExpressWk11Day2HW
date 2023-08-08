acd4b1753bbd622047ddcc49d2ec3f15d206619f
Lynette <lynettecargo@Lynettes-Air.home>
2023-08-08 15:21:20 -0500

    git init

---
+++ index.js
@@ -0,0 +1,20 @@
+// express
+const express = require('express');
+const app = express();
+
+
+
+
+
+
+
+
+
+
+
+
+// ---------------------------------------------------------------------------------------
+// server
+app.listen('3000', (req,res)=>{
+    console.log('Server at port 3000')
+})
